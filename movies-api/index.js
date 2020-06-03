3bc149c894e16889e1a7265ffcdca013c8930d37
HERNAN PINOS <hernan.pinos@outlook.com>
2020-06-03 15:26:50 -0500

    Creacion del CRUD en Express.js list

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,18 +3,9 @@ const app = express();
 
 const { config } = require('./config/index');
 
-app.get('/', function (req, res) {
-    res.send('hello world');
-});
+const moviesApi = require('./routes/movies.js');
 
-app.get('/json', function (req, res) {
-    res.json({ hello: 'world' });
-});
-
-app.get('/challenge/:year', function (req, res) {
-    let { year } = req.params;
-    res.json({ year: `${year}`, result: `${((year % 4 === 0 && year !== 0) || year % 400 === 0) ? 'si es bisiesto' : 'no es bisiesto'}` });
-});
+moviesApi(app);
 
 app.listen(config.port, function () {
     console.log(`Listening http://localhost:${config.port}`);
---
+++ movies-api/utils/mocks/movies.js
@@ -0,0 +1,137 @@
+const moviesMock = [{
+    "id": "ac37bfb4-ee17-4c0d-8c3a-f358434d5ce6",
+    "title": "Good Lawyer's Wife, A (Baramnan gajok)",
+    "year": 2013,
+    "description": "http://dummyimage.com/179x179.bmp/dddddd/000000",
+    "duration": 5,
+    "contentRating": null,
+    "source": "http://zdnet.com/faucibus/orci.aspx?sit=nulla&amet=elit&justo=ac",
+    "tags": [
+        "Drama|Romance",
+        "Drama",
+        "Animation|Children"
+    ]
+}, {
+    "id": "7283fc9d-f23c-4237-9385-52cfacdfe270",
+    "title": "Film ist. 7-12",
+    "year": 2006,
+    "description": "http://dummyimage.com/196x197.jpg/5fa2dd/ffffff",
+    "duration": 83,
+    "contentRating": null,
+    "source": "https://cloudflare.com/amet/sem/fusce.jpg?tortor=nisi&sollicitudin=at&mi=nibh&sit=in&amet=hac&lobortis=habitasse&sapien=platea&sapien=dictumst&non=aliquam&mi=augue&integer=quam&ac=sollicitudin&neque=vitae",
+    "tags": [
+        "Crime|Drama|Mystery|Thriller"
+    ]
+}, {
+    "id": "7e4b8601-88c9-43af-8390-7654fce60928",
+    "title": "Night in Heaven, A",
+    "year": 2013,
+    "description": "http://dummyimage.com/113x163.jpg/5fa2dd/ffffff",
+    "duration": 90,
+    "contentRating": null,
+    "source": "https://wsj.com/in/faucibus/orci/luctus/et/ultrices/posuere.json?vestibulum=habitasse&velit=platea&id=dictumst&pretium=etiam&iaculis=faucibus&diam=cursus&erat=urna&fermentum=ut&justo=tellus&nec=nulla&condimentum=ut&neque=erat&sapien=id&placerat=mauris&ante=vulputate&nulla=elementum&justo=nullam&aliquam=varius&quis=nulla&turpis=facilisi&eget=cras&elit=non&sodales=velit&scelerisque=nec&mauris=nisi&sit=vulputate&amet=nonummy&eros=maecenas&suspendisse=tincidunt&accumsan=lacus&tortor=at&quis=velit&turpis=vivamus&sed=vel&ante=nulla&vivamus=eget&tortor=eros&duis=elementum&mattis=pellentesque&egestas=quisque&metus=porta&aenean=volutpat&fermentum=erat&donec=quisque&ut=erat&mauris=eros&eget=viverra&massa=eget&tempor=congue&convallis=eget&nulla=semper&neque=rutrum&libero=nulla&convallis=nunc&eget=purus&eleifend=phasellus&luctus=in&ultricies=felis&eu=donec&nibh=semper&quisque=sapien&id=a&justo=libero&sit=nam&amet=dui&sapien=proin&dignissim=leo&vestibulum=odio&vestibulum=porttitor&ante=id&ipsum=consequat&primis=in&in=consequat&faucibus=ut&orci=nulla&luctus=sed&et=accumsan&ultrices=felis&posuere=ut&cubilia=at&curae=dolor&nulla=quis&dapibus=odio&dolor=consequat&vel=varius&est=integer&donec=ac&odio=leo&justo=pellentesque&sollicitudin=ultrices&ut=mattis&suscipit=odio&a=donec&feugiat=vitae&et=nisi&eros=nam&vestibulum=ultrices&ac=libero&est=non&lacinia=mattis",
+    "tags": [
+        "Comedy|Drama"
+    ]
+}, {
+    "id": "c63741b5-6719-4036-9691-7e35fb5d6487",
+    "title": "Under the Roofs of Paris (Sous les toits de Paris)",
+    "year": 2005,
+    "description": "http://dummyimage.com/145x114.jpg/cc0000/ffffff",
+    "duration": 61,
+    "contentRating": null,
+    "source": "https://quantcast.com/mauris/sit/amet/eros/suspendisse/accumsan/tortor.html?magna=fusce&bibendum=congue&imperdiet=diam&nullam=id&orci=ornare&pede=imperdiet&venenatis=sapien&non=urna&sodales=pretium&sed=nisl&tincidunt=ut&eu=volutpat&felis=sapien&fusce=arcu&posuere=sed&felis=augue&sed=aliquam&lacus=erat&morbi=volutpat&sem=in&mauris=congue&laoreet=etiam&ut=justo&rhoncus=etiam&aliquet=pretium&pulvinar=iaculis&sed=justo&nisl=in&nunc=hac&rhoncus=habitasse&dui=platea&vel=dictumst&sem=etiam&sed=faucibus&sagittis=cursus&nam=urna&congue=ut&risus=tellus&semper=nulla&porta=ut&volutpat=erat&quam=id&pede=mauris&lobortis=vulputate&ligula=elementum&sit=nullam&amet=varius&eleifend=nulla&pede=facilisi&libero=cras&quis=non&orci=velit&nullam=nec&molestie=nisi&nibh=vulputate&in=nonummy&lectus=maecenas&pellentesque=tincidunt&at=lacus&nulla=at&suspendisse=velit&potenti=vivamus&cras=vel&in=nulla&purus=eget&eu=eros&magna=elementum&vulputate=pellentesque&luctus=quisque",
+    "tags": [
+        "Comedy|Drama"
+    ]
+}, {
+    "id": "aa1a7af9-f0db-4e97-8a58-995206892313",
+    "title": "When Trumpets Fade",
+    "year": 2009,
+    "description": "http://dummyimage.com/174x165.bmp/5fa2dd/ffffff",
+    "duration": 63,
+    "contentRating": null,
+    "source": "http://census.gov/mauris/ullamcorper/purus/sit/amet/nulla/quisque.js?massa=vel&donec=ipsum&dapibus=praesent&duis=blandit&at=lacinia&velit=erat&eu=vestibulum&est=sed&congue=magna&elementum=at&in=nunc&hac=commodo&habitasse=placerat&platea=praesent&dictumst=blandit&morbi=nam&vestibulum=nulla&velit=integer&id=pede&pretium=justo&iaculis=lacinia&diam=eget&erat=tincidunt&fermentum=eget&justo=tempus&nec=vel&condimentum=pede&neque=morbi&sapien=porttitor&placerat=lorem&ante=id&nulla=ligula&justo=suspendisse&aliquam=ornare&quis=consequat&turpis=lectus&eget=in&elit=est&sodales=risus&scelerisque=auctor&mauris=sed&sit=tristique&amet=in&eros=tempus&suspendisse=sit&accumsan=amet&tortor=sem&quis=fusce&turpis=consequat&sed=nulla&ante=nisl&vivamus=nunc&tortor=nisl&duis=duis&mattis=bibendum&egestas=felis&metus=sed&aenean=interdum&fermentum=venenatis&donec=turpis&ut=enim&mauris=blandit&eget=mi&massa=in&tempor=porttitor&convallis=pede&nulla=justo&neque=eu&libero=massa&convallis=donec&eget=dapibus&eleifend=duis&luctus=at&ultricies=velit&eu=eu&nibh=est&quisque=congue&id=elementum&justo=in&sit=hac&amet=habitasse&sapien=platea&dignissim=dictumst&vestibulum=morbi&vestibulum=vestibulum&ante=velit&ipsum=id&primis=pretium&in=iaculis&faucibus=diam&orci=erat&luctus=fermentum&et=justo",
+    "tags": [
+        "Drama|Romance",
+        "Children|Drama",
+        "Horror",
+        "Action|Adventure|Animation"
+    ]
+}, {
+    "id": "b698cb86-c5ba-40fe-a9f0-ff3fff1df9c4",
+    "title": "Forever",
+    "year": 1992,
+    "description": "http://dummyimage.com/219x211.png/ff4444/ffffff",
+    "duration": 49,
+    "contentRating": null,
+    "source": "http://comcast.net/ultricies/eu/nibh/quisque/id/justo.aspx?lobortis=nulla&convallis=nunc&tortor=purus&risus=phasellus&dapibus=in&augue=felis&vel=donec&accumsan=semper&tellus=sapien&nisi=a&eu=libero&orci=nam&mauris=dui&lacinia=proin&sapien=leo&quis=odio&libero=porttitor&nullam=id&sit=consequat&amet=in&turpis=consequat&elementum=ut&ligula=nulla&vehicula=sed&consequat=accumsan&morbi=felis&a=ut&ipsum=at&integer=dolor&a=quis&nibh=odio&in=consequat&quis=varius&justo=integer&maecenas=ac&rhoncus=leo&aliquam=pellentesque&lacus=ultrices&morbi=mattis&quis=odio&tortor=donec&id=vitae&nulla=nisi&ultrices=nam&aliquet=ultrices&maecenas=libero&leo=non&odio=mattis&condimentum=pulvinar&id=nulla&luctus=pede&nec=ullamcorper&molestie=augue&sed=a&justo=suscipit&pellentesque=nulla&viverra=elit&pede=ac&ac=nulla&diam=sed&cras=vel&pellentesque=enim&volutpat=sit&dui=amet&maecenas=nunc&tristique=viverra&est=dapibus&et=nulla&tempus=suscipit&semper=ligula&est=in&quam=lacus&pharetra=curabitur&magna=at&ac=ipsum&consequat=ac",
+    "tags": [
+        "Comedy|Romance",
+        "Drama",
+        "Horror",
+        "Comedy"
+    ]
+}, {
+    "id": "36048583-bf64-4f59-bbc5-e45bb7473791",
+    "title": "Hi Tessa (Czesc Tereska)",
+    "year": 1999,
+    "description": "http://dummyimage.com/250x175.bmp/cc0000/ffffff",
+    "duration": 77,
+    "contentRating": null,
+    "source": "https://google.co.jp/congue/risus/semper/porta.aspx?est=rhoncus&et=aliquam&tempus=lacus&semper=morbi&est=quis&quam=tortor&pharetra=id&magna=nulla&ac=ultrices&consequat=aliquet&metus=maecenas&sapien=leo&ut=odio&nunc=condimentum&vestibulum=id&ante=luctus&ipsum=nec&primis=molestie&in=sed&faucibus=justo&orci=pellentesque&luctus=viverra&et=pede&ultrices=ac&posuere=diam&cubilia=cras&curae=pellentesque&mauris=volutpat&viverra=dui&diam=maecenas&vitae=tristique&quam=est&suspendisse=et&potenti=tempus&nullam=semper&porttitor=est&lacus=quam&at=pharetra&turpis=magna&donec=ac&posuere=consequat&metus=metus&vitae=sapien&ipsum=ut&aliquam=nunc&non=vestibulum&mauris=ante&morbi=ipsum&non=primis&lectus=in&aliquam=faucibus&sit=orci&amet=luctus&diam=et&in=ultrices&magna=posuere&bibendum=cubilia&imperdiet=curae&nullam=mauris&orci=viverra&pede=diam&venenatis=vitae&non=quam&sodales=suspendisse&sed=potenti",
+    "tags": [
+        "Horror|Sci-Fi|Thriller",
+        "Mystery|Sci-Fi",
+        "Animation|Children|Comedy",
+        "Drama"
+    ]
+}, {
+    "id": "a006908d-c2f9-47dd-9962-84eaaaa415f1",
+    "title": "World Apart, A",
+    "year": 2002,
+    "description": "http://dummyimage.com/185x226.bmp/5fa2dd/ffffff",
+    "duration": 83,
+    "contentRating": null,
+    "source": "http://dmoz.org/lacinia/eget/tincidunt.xml?maecenas=rhoncus&leo=sed&odio=vestibulum&condimentum=sit&id=amet&luctus=cursus&nec=id&molestie=turpis&sed=integer&justo=aliquet&pellentesque=massa&viverra=id&pede=lobortis&ac=convallis",
+    "tags": [
+        "Comedy|Crime",
+        "Crime|Drama",
+        "Action|Thriller",
+        "Romance",
+        "Horror|Sci-Fi"
+    ]
+}, {
+    "id": "f9061698-ed60-4f62-8ffc-833b8d2434fc",
+    "title": "Gregory Crewdson: Brief Encounters",
+    "year": 1995,
+    "description": "http://dummyimage.com/105x250.jpg/dddddd/000000",
+    "duration": 64,
+    "contentRating": null,
+    "source": "http://cbslocal.com/ipsum/primis.xml?nec=morbi&condimentum=a&neque=ipsum&sapien=integer&placerat=a&ante=nibh&nulla=in&justo=quis&aliquam=justo&quis=maecenas&turpis=rhoncus&eget=aliquam&elit=lacus&sodales=morbi&scelerisque=quis&mauris=tortor&sit=id&amet=nulla&eros=ultrices&suspendisse=aliquet&accumsan=maecenas&tortor=leo&quis=odio&turpis=condimentum&sed=id&ante=luctus&vivamus=nec&tortor=molestie&duis=sed&mattis=justo&egestas=pellentesque&metus=viverra&aenean=pede&fermentum=ac&donec=diam&ut=cras&mauris=pellentesque&eget=volutpat&massa=dui&tempor=maecenas&convallis=tristique&nulla=est&neque=et&libero=tempus",
+    "tags": [
+        "Comedy",
+        "Horror",
+        "Western",
+        "Animation|Drama|Romance",
+        "Comedy"
+    ]
+}, {
+    "id": "42013451-733d-45cb-82d4-24acebe571b0",
+    "title": "Casper",
+    "year": 1999,
+    "description": "http://dummyimage.com/189x211.bmp/ff4444/ffffff",
+    "duration": 59,
+    "contentRating": null,
+    "source": "http://taobao.com/ut/nunc/vestibulum.xml?vel=duis&ipsum=mattis&praesent=egestas&blandit=metus&lacinia=aenean&erat=fermentum&vestibulum=donec&sed=ut&magna=mauris&at=eget&nunc=massa&commodo=tempor&placerat=convallis&praesent=nulla&blandit=neque&nam=libero&nulla=convallis&integer=eget&pede=eleifend&justo=luctus&lacinia=ultricies&eget=eu&tincidunt=nibh&eget=quisque&tempus=id&vel=justo&pede=sit&morbi=amet&porttitor=sapien&lorem=dignissim&id=vestibulum&ligula=vestibulum&suspendisse=ante&ornare=ipsum&consequat=primis&lectus=in&in=faucibus&est=orci&risus=luctus&auctor=et&sed=ultrices&tristique=posuere&in=cubilia&tempus=curae&sit=nulla&amet=dapibus&sem=dolor&fusce=vel&consequat=est&nulla=donec&nisl=odio&nunc=justo&nisl=sollicitudin&duis=ut&bibendum=suscipit&felis=a&sed=feugiat&interdum=et&venenatis=eros&turpis=vestibulum&enim=ac&blandit=est&mi=lacinia&in=nisi&porttitor=venenatis&pede=tristique&justo=fusce&eu=congue",
+    "tags": [
+        "(no genres listed)",
+        "Drama",
+        "Action|Adventure|Animation|Comedy|Fantasy",
+        "Comedy|Romance"
+    ]
+}];
+
+module.exports = {
+    moviesMock
+}
