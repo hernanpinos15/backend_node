d99b7f5b117b1e75b3337cd475a50012e5474abe
HERNAN PINOS <hernan.pinos@outlook.com>
2020-06-03 09:07:24 -0500

    Sisema operativo y sistema de archivos

---
+++ learning-node/so-archivos/os.js
@@ -0,0 +1,8 @@
+const os = require('os');
+
+// console.log("CPU info", os.cpus());
+// console.log("Ip address", os.networkInterfaces().en0.map(i => i.address));
+// console.log("free memory", os.freemem());
+// console.log("Type", os.type());
+// console.log("SP version", os.release());
+console.log("Usr info", os.userInfo());
