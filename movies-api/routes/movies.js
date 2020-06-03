df8d726a4453d22c88ea389b24d602beeb46c3c5
HERNAN PINOS <hernan.pinos@outlook.com>
2020-06-03 15:47:36 -0500

    Creacion de metodos del CRUD y creacion del archivo .gitignore

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -18,6 +18,58 @@ function moviesApi(app){
             next(err);
         }
     });
+
+    router.get("/:movieID", async function(req, res, next){
+        try{
+            const movies = await Promise.resolve(moviesMock[0]);
+
+            res.status(200).json({
+                data: movies,
+                message: 'movie retrieved'
+            })
+        }catch(err){
+            next(err);
+        }
+    });
+
+    router.post("/", async function(req, res, next){
+        try{
+            const createMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(201).json({
+                data: createMovieId,
+                message: 'movie created'
+            })
+        }catch(err){
+            next(err);
+        }
+    });
+
+    router.put("/:movieId", async function(req, res, next){
+        try{
+            const updateMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(200).json({
+                data: updateMovieId,
+                message: 'movie updated'
+            })
+        }catch(err){
+            next(err);
+        }
+    });
+
+    router.delete("/:movieId", async function(req, res, next){
+        try{
+            const deleteMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(200).json({
+                data: deleteMovieId,
+                message: 'movie deleted'
+            })
+        }catch(err){
+            next(err);
+        }
+    });
 }
 
 module.exports = moviesApi;
