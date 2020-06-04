f065707c12a6db2bbaa75beeab5457b8ac163fe1
HERNAN PINOS <hernan.pinos@outlook.com>
2020-06-03 21:33:42 -0500

    Implementacion de las acciones de MongoDB

--- movies-api/lib/mongo.js
+++ movies-api/lib/mongo.js
@@ -7,17 +7,17 @@ const DB_NAME = config.dbName;
 
 const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`;
 
-class MongoLib{
-    constructor(){
-        this.client = new MongoClient(MONGO_URI, {useNewUrlParser: true});
+class MongoLib {
+    constructor() {
+        this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
         this.dbName = DB_NAME;
     }
 
-    connect(){
-        if(!MongoLib.connection){
-            MongoLib.connection = new Promise((resolve, reject) =>{
-                this.client.connect(err =>{
-                    if(err){
+    connect() {
+        if (!MongoLib.connection) {
+            MongoLib.connection = new Promise((resolve, reject) => {
+                this.client.connect(err => {
+                    if (err) {
                         reject(err);
                     }
                     console.log('Connected succesfully to mongo');
@@ -26,6 +26,46 @@ class MongoLib{
             })
         }
         return MongoLib.connection;
+    }
+    getAll(collection, query) {
+        return this.connect().then(db => {
+            return db
+                .collection(collection)
+                .find(query)
+                .toArray();
+        });
+    }
+
+    get(collection, id) {
+        return this.connect().then(db => {
+            return db.collection(collection).findOne({ _id: ObjectId(id) });
+        });
+    }
+
+    create(collection, data) {
+        return this.connect()
+            .then(db => {
+                return db.collection(collection).insertOne(data);
+            })
+            .then(result => result.insertedId);
+    }
+
+    update(collection, id, data) {
+        return this.connect()
+            .then(db => {
+                return db
+                    .collection(collection)
+                    .updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true });
+            })
+            .then(result => result.upsertedId || id);
+    }
+
+    delete(collection, id) {
+        return this.connect()
+            .then(db => {
+                return db.collection(collection).deleteOne({ _id: ObjectId(id) });
+            })
+            .then(() => id);
     }
 }
 
