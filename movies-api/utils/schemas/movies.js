168f28f29508fe1f83c70931960fbc6bf1544bec
HERNAN PINOS <hernan.pinos@outlook.com>
2020-06-04 12:28:03 -0500

    Probando la validacion de nuestros endpoints

--- movies-api/utils/schemas/movies.js
+++ movies-api/utils/schemas/movies.js
@@ -1,14 +1,14 @@
 const joi = require('@hapi/joi');
 
 const movieIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/)
-const movieTitleSchema = joi.string.max(80);
-const movieYearSchema = joi.number.min(1888).max(2077);
-const movieCoverSchema = joi.string.url();
-const movieDescriptionSchema = joi.string.max(300);
-const movieDurationSchema = joi.string.min(1).max(300);
-const movieContentRattingSchema = joi.string.max(5);
-const movieSourceSchema = joi.string.url();
-const movieTagsSchema = joi.array().items(joi.string.max(50));
+const movieTitleSchema = joi.string().max(80);
+const movieYearSchema = joi.number().min(1888).max(2077);
+const movieCoverSchema = joi.string().uri();
+const movieDescriptionSchema = joi.string().max(300);
+const movieDurationSchema = joi.number().min(1).max(300);
+const movieContentRatingSchema = joi.string().max(5);
+const movieSourceSchema = joi.string().uri();
+const movieTagsSchema = joi.array().items(joi.string().max(50));
 
 const createMovieSchema = {
     title: movieTitleSchema.required(),
@@ -16,7 +16,7 @@ const createMovieSchema = {
     cover: movieCoverSchema.required(),
     description: movieDescriptionSchema.required(),
     duration: movieDurationSchema.required(),
-    contentRatting: movieContentRattingSchema.required(),
+    contentRating: movieContentRatingSchema.required(),
     source: movieSourceSchema.required(),
     tags: movieTagsSchema
 };
@@ -27,7 +27,7 @@ const updateMovieSchema = {
     cover: movieCoverSchema,
     description: movieDescriptionSchema,
     duration: movieDurationSchema,
-    contentRatting: movieContentRattingSchema,
+    contentRating: movieContentRatingSchema,
     source: movieSourceSchema,
     tags: movieTagsSchema
 };
