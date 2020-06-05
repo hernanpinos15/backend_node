26243355a6318d2e0448676116b8d8b216527849
HERNAN PINOS <hernan.pinos@outlook.com>
2020-06-05 17:03:15 -0500

    Implementacion de cahe antes de subir a produccion

--- movies-api/utils/cacheResponde.js
+++ movies-api/utils/cacheResponde.js
@@ -1,9 +1,15 @@
 const { config } = require('../config');
 
-function cacheResponse(res,seconds){
-    if(!config.dev){
-        res.set("Cache-Control", `public, max-age=${seconds}`);
-    }
+function cacheResponse(res, seconds) {
+    res.set('Cache-Control', `public, max-age=${seconds}`);
+    console.log(`DEV: ${config.dev} - START: ${config.start}`);
+    // if (config.start === "production") {
+    //     res.set('Cache-Control', `public, max-age=${seconds}`);
+    //     console.log('si');
+    // } else {
+    //     console.log('no');
+    //     res.set('Cache-Control', `public, max-age=${seconds}`);
+    // }
 }
 
-module.exports =cacheResponse;
+module.exports = cacheResponse;
